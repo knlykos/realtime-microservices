94d6800f1d038df110b1ff003bc1d6083774c4a9
Nefi López García <nlopezg87@gmail.com>
2019-08-10 12:26:57 -0700

    actualizacion de servidor tiempo real

--- httprest/server.ts
+++ httprest/server.ts
@@ -1,15 +1,30 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import { connect, NatsConnectionOptions, Payload } from 'ts-nats';
+import * as bodyParser from 'body-parser';
 const app = express();
 const port = 3001;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.text());
+
 app.post('/', async (req: Request, res: Response) => {
   let nc = await connect({ servers: ['nats://172.17.03:4222'] });
+  console.log(req.body);
+  var slData = req.body as {
+    clientId: number;
+    orderId: number;
+    surtidorId: number;
+    customerId: number;
+  };
 
-  var slData = { subscription: 'Listado de toda la orden' };
-
-  nc.publish('greetings', JSON.stringify(slData));
+  nc.publish(
+    `{"orderId": ${slData.orderId}, "surtidorId": ${slData.surtidorId}, "customerId": ${
+      slData.customerId
+    }}`,
+    JSON.stringify(slData)
+  );
 
   res.send('Respuesta del POST');
 });
