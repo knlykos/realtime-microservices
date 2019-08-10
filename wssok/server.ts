508867c9f6bdb1a771799da8c9342b09e690232c
Nefi López García <nlopezg87@gmail.com>
2019-08-10 12:26:33 -0700

    ruta de servidor tiempo real

--- wssok/server.ts
+++ wssok/server.ts
@@ -13,37 +13,57 @@ const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws: WebSocket, req: http.IncomingMessage) => {
-  //connection is up, let's add a simple simple event
-
-  ws.on('message', (message: string) => {
-    var event = JSON.parse(message);
-    ws.emit(event.type, event.payload);
-    //log the received message and send it back to the client
-
-    // let nc = connect({ servers: ['nats://172.17.03:4222'] }).then(res => {
-    //   res.subscribe('greetings', (err, msg) => {
-    //     wss.clients.forEach(clients => {
-    //       if (clients.readyState === WebSocket.OPEN) {
-    //         clients.send(msg.data);
-    //       }
-    //     });
-    //     console.log(msg);
-    //   });
-    // });
-    // ws.send(`Hello, you sent -> ${message}`);
-  }).on('authenticate', e => {
-    ws.send(`Hello, you sent -> ${JSON.stringify(e)}`);
-
-    wsClients[e.clientId] = ws;
-    console.log(wsClients);
-    wss.clients.forEach(v => {
-      console.log(wsClients[1].send('hey2'));
+function nc() {
+  return connect({ servers: ['nats://172.17.03:4222'] });
+}
+wss.on('connection', async (ws: WebSocket, req: http.IncomingMessage) => {
+  // nc().then(res => {
+  //   res.subscribe('update-orders', (err, msg) => {});
+  // });
+  ws.on('message', (msg: string) => {
+    var slData = JSON.parse(msg) as {
+      route: {
+        clientId: number;
+        orderId: number;
+        surtidorId: number;
+        customerId: number;
+      };
+    };
+    nc().then(res => {
+      res.subscribe(
+        `{"orderId": ${slData.route.orderId}, "surtidorId": ${
+          slData.route.surtidorId
+        }, "customerId": ${slData.route.customerId}}`,
+        (err, msg) => {
+          console.log(err);
+          console.log(msg);
+        }
+      );
     });
   });
-
+  // res.subscribe('update-orders', (err, msg) => {
+  //   ws.on('message', (message: string) => {
+  //     var event = JSON.parse(message);
+  //     ws.emit(msg.data.type, 'test');
+  //   });
+  // });
+  //connection is up, let's add a simple simple event
+  // ws.on('message', (message: string) => {
+  //   var event = JSON.parse(message);
+  //   ws.emit(event.type, event.payload);
+  //   //log the received message and send it back to the client
+  //   // ws.send(`Hello, you sent -> ${message}`);
+  // }).on('authenticate', e => {
+  //   ws.send(`Hello, you sent -> ${JSON.stringify(e)}`);
+  //   wsClients[e.clientId] = ws;
+  //   wsClients.forEach((v, i, a) => {
+  //     if (v.readyState === v.OPEN) {
+  //       v.send('hey' + i);
+  //     }
+  //   });
+  // });
   //send immediatly a feedback to the incoming connection
-  ws.send('Hi there, I am a WebSocket server');
+  // ws.send('Hi there, I am a WebSocket server');
 });
 
 //start our server
