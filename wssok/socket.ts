8fc3c767908eef54ea86c8c764502ca2f01d2279
Nefi López García <nlopezg87@gmail.com>
2019-11-05 17:03:52 -0800

    socket io implementado

---
+++ wssok/socket.ts
@@ -0,0 +1,71 @@
+import * as socketio from 'socket.io';
+import { connect, NatsConnectionOptions, Payload, Client } from 'ts-nats';
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var io: SocketIO.Server = require('socket.io')(http);
+
+function nc(): Promise<Client> {
+  return connect({ servers: ['nats://172.17.03:4222'] });
+}
+
+// app.all('/*', function(req, res, next) {
+//   res.header('Set-Cookie: cross-site-cookie=name; SameSite=None; Secure');
+//   next();
+// });
+
+io.on('connection', function(socket: socketio.Socket) {
+  socket.on('ksp110-sls', data => {
+    socket.join(data.room);
+    nc().then(res => {
+      res.subscribe(data.room, (err, msg) => {
+        // console.log(msg);
+        io.sockets.in(msg.subject).emit('ksp110-sls', JSON.parse(msg.data));
+        res.close();
+      });
+    });
+  });
+
+  socket.on('ksp110-sl', data => {
+    console.log(data);
+    socket.join(data.room);
+    nc().then(res => {
+      res.subscribe(data.room, (err, msg) => {
+        io.sockets.in(msg.subject).emit('ksp110-sl', JSON.parse(msg.data));
+        res.close();
+      });
+    });
+  });
+
+  // ksp110 - slp;
+  socket.on('ksp110-slp', data => {
+    socket.join(data.room);
+    nc().then(res => {
+      res.subscribe(data.room, (err, msg) => {
+        console.log(msg);
+        io.sockets.in(msg.subject).emit('ksp110-slp', JSON.parse(msg.data));
+        res.close();
+      });
+    });
+  });
+
+  socket.on('ksp110-slsag', data => {
+    socket.join(data.room);
+    console.log('ksp110-slsag', data.room);
+    nc().then(res => {
+      res.subscribe(data.room, (err, msg) => {
+        console.log(msg);
+        io.sockets.in(msg.subject).emit('ksp110-slsag', JSON.parse(msg.data));
+        res.close();
+      });
+    });
+  });
+});
+
+// ksp110.slsagp.orden.${this.kSpJs110.param_in.pidOrden}.folio.${this.kSpJs110.param_in.pFolio}
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
+
+// io.listen(3000, { cookie: false, origins: '*' });
